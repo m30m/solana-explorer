e01db852f9ce808801163a7c8811fb835e84ae8d
Amin Moghaddam <amin@pyth.network>
2024-06-12 13:53:44 +0200

    Update to 0.30.0

--- app/utils/anchor.tsx
+++ app/utils/anchor.tsx
@@ -11,7 +11,7 @@ import { ChevronDown, ChevronUp, CornerDownRight } from 'react-feather';
 import ReactJson from 'react-json-view';
 
 export function getAnchorProgramName(program: Program | null): string | undefined {
-    return program ? snakeToTitleCase(program.idl.name) : undefined;
+    return program ? snakeToTitleCase(program.idl.metadata.name) : undefined;
 }
 
 export function AnchorProgramName({
